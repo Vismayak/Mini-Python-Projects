7614d0212f74c41baf755f3eeecedc6f76afb87b
Vismayak Mohanarajan <vismayak97@gmail.com>
2018-09-16 00:52:34 -0500

    comments for first mini

--- requeststests.py
+++ requeststests.py
@@ -1,3 +1,5 @@
+# This piece of code helps create an imperfect copy of the page you wish to emulate, try https://www.facebook.com for best results
+
 import requests
 
 name = input("Enter the website you want to make an imperfect copy of (use https://) -   ")
